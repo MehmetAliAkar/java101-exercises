4f04eb7998f3a8532fbae13a1a6b5b62252dd8e4
Mehmet Ali AKAR <97257481+MehmetAliAkar@users.noreply.github.com>
2023-06-25 13:47:22 +0300

    Update sinifiGecmeDurumu.java

--- sinifiGecmeDurumu.java
+++ sinifiGecmeDurumu.java
@@ -4,23 +4,50 @@ public class sinifiGecmeDurumu {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int mat,fiz,kimya,turkce,muzik;
+        int dersSayisi=5;
 
         System.out.printf("Mat notunu giriniz : ");
         mat=input.nextInt();
+        if(mat>=100 || mat<=0)
+        {
+            mat=0;
+            dersSayisi--;
+        }
 
         System.out.printf("Fizik notunu giriniz : ");
         fiz=input.nextInt();
+        if(fiz>=100 || fiz<=0)
+        {
+            fiz=0;
+            dersSayisi--;
+        }
 
         System.out.printf("Kimya notunu giriniz : ");
         kimya=input.nextInt();
+        if(kimya>=100 || kimya<=0)
+        {
+            kimya=0;
+            dersSayisi--;
+        }
 
         System.out.printf("Turkce notunu giriniz : ");
         turkce=input.nextInt();
+        if(turkce>=100 || turkce<=0)
+        {
+            turkce=0;
+            dersSayisi--;
+        }
 
         System.out.printf("Muzik notunu giriniz : ");
         muzik=input.nextInt();
+        if(muzik>=100 || muzik<=0)
+        {
+            muzik=0;
+            dersSayisi--;
+        }
 
-        double ort=(fiz+mat+turkce+muzik+kimya)/5;
+        double ort=(fiz+mat+turkce+muzik+kimya)/dersSayisi;
+        System.out.println("ders sayisi : "+dersSayisi);
 
         if(ort>=55){
             System.out.println("Sinifi gectiniz!");
@@ -30,6 +57,5 @@ public class sinifiGecmeDurumu {
             System.out.println("Sinifi geçemediniz!");
             System.out.println("Ortalamanız : "+ort);}
         }
-
     }
 
